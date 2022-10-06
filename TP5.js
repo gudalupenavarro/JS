41029b5174a9d18bdcffac20525e0d9c0dccfa05
guadalupenavarro <guada.navarro.epet20@gmail.com>
2022-10-06 14:45:18 -0300

    Comiteando la primera region

---
+++ TP5.js
@@ -0,0 +1,19 @@
+
+//region
+var n1=10;
+let numMulti=10;
+var multi= 0;
+
+var n1=10;
+let numDivi=10;
+var div=0;
+
+var n1=10;
+let numSuma=10;
+var suma= 0;
+
+var n1=10;
+let numResta=10;
+var resta= 0;
+
+//endregion
